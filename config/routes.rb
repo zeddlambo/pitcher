59a08cc6c30e46234f5e3e5bc4783fdf5d0f85fa
Zac Campbell <zac2013@gmail.com>
2013-10-25 07:53:20 -0700

    changes to user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Pinteresting::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "advertisers#index"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
