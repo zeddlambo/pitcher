4903d8642ad999c32a7eef06fdcce41a7cf3872f
Zac Campbell <zac2013@gmail.com>
2013-10-25 15:34:19 -0700

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-end
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_token
 
   has_many :advertisers
 
