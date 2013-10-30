556dfc280c6251fdd5da9db9c5c3ded847ebed2e
Zac Campbell <zac2013@gmail.com>
2013-10-29 23:34:52 -0700

    download csv cases

--- app/controllers/advertisers_controller.rb
+++ app/controllers/advertisers_controller.rb
@@ -8,7 +8,7 @@ class AdvertisersController < ApplicationController
   # GET /advertisers
   # GET /advertisers.json
   def index
-    @advertisers = Advertiser.all
+    @advertisers = current_user.advertisers
     respond_to do |format|
       format.html
       format.csv { send_data @advertisers.to_csv }
