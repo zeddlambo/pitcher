99072a0915931cc89868652ed7e9904c2ed7e1da
Zac Campbell <zac2013@gmail.com>
2013-11-05 12:01:28 -0800

    show all cases

--- app/controllers/advertisers_controller.rb
+++ app/controllers/advertisers_controller.rb
@@ -8,7 +8,7 @@ class AdvertisersController < ApplicationController
   # GET /advertisers
   # GET /advertisers.json
   def index
-    @advertisers = current_user.advertisers
+    @advertisers = current_user.advertisers.today
     respond_to do |format|
       format.html
       format.csv { send_data @advertisers.to_csv }
