1d6110ce3826405621521a550284693a70d28063
Zac Campbell <zac2013@gmail.com>
2013-11-19 16:56:14 -0800

    added finished

--- app/controllers/advertisers_controller.rb
+++ app/controllers/advertisers_controller.rb
@@ -52,7 +52,7 @@ class AdvertisersController < ApplicationController
   def update
     respond_to do |format|
       if @advertiser.update(advertiser_params)
-        format.html { redirect_to request.env['HTTP_REFERER']}
+        format.html { redirect_to advertiser_path}
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
